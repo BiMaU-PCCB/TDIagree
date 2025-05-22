42dc9040bebf829efa8e46fb9e43064c7d47bb77
AnnaFelip-Badia <annafelipibadia@gmail.com>
2025-05-22 18:21:09 +0200

    Add files via upload

--- R/TDI.Escaramis.R
+++ R/TDI.Escaramis.R
@@ -2,101 +2,148 @@
 #'
 #' @param data.long name of the dataset containing at least 3 columns (measurement, subject effect, rater effect).
 #' @param y measurement column name.
-#' @param id subject effect column name. Must be a factor.
-#' @param met rater effect column name. Must be a factor.
-#' @param rep replication effect column name. Must be a factor. When there are no replicas rep = NA
-#'            Default is NA.
-#' @param p A value or vector of the percentiles for the TDI.
-#' @param choose.model logical asking if the minimum AIC model should be chosen no matter the value of `int`.
+#' @param id subject effect column name.
+#' @param met rater effect column name.
+#' @param rep replication effect column name.
+#' @param p A value or vector of the probabilities for the TDI.
+#' @param ub logical asking whether the UBs should be computed.
+#'           Default is TRUE.
+#' @param choose.model logical asking if the minimum AIC model should be chosen no matter the value of int.
+#'                     Default is TRUE.
 #' @param int if FALSE, a model with no interaction is fitted; if TRUE, a model with interaction is fitted.
 #'            Default is FALSE. Only makes sense for data with repetition
-#' @param tol tolerance
+#' @param add.es name of the columns in data that should be added to the model.
+#'               Default is NULL.
+#' @param tol tolerance.
 #'            Default is 10^(-8)
-#' @param alpha confidence level for the TDI's upper bounds.
+#' @param alpha confidence level for inference on the TDI.
 #'              Default is 0.05.
 #'
 #' @importFrom nlme lme pdBlocked pdIdent getVarCov
-#' @importFrom stats AIC qchisq pnorm qnorm qt
+#' @importFrom stats AIC qchisq pnorm qnorm qt as.formula
 #'
 #' @noRd
 
 
-TDI.Escaramis <- function(data.long, y, id, met, rep = NA, p,
-                          choose.model = T, tol = 10^(-8), int = F, alpha = 0.05){
+TDI.Escaramis <- function(data.long, rep, p, ub = TRUE,
+                          choose.model = TRUE, tol = 10^(-8), int = F, add.es = NULL, alpha = 0.05){
 
-  if (!is.na(NA)) {
-    m =  length(levels(data.long[, rep]))
+  if (!is.na(rep)) {
+    m =  nlevels(data.long[, "rep"])
   } else{
     rep <- NA
     m = 1
   }
 
-  # Number of subjects
   ns <- length(unique(data.long$id))
 
-  if(choose.model){
-    if(!is.na(rep)){
-      model.no.int <- lme(y~met, data = data.long, random = ~1|id, method = "REML")
-      model.int <- lme(y~met, data = data.long,
-                       random = list(id = pdBlocked(list(~1, pdIdent(form = ~-1+met)))),
-                       method = "REML")
-      if(AIC(model.no.int) <= AIC(model.int)){
-        model <- model.no.int
-        md <- abs(summary(model)$coefficients$fixed[2])
-        sd <- sqrt(2)*model$sigma # Standard deviation of the difference
-        df <- 2*ns*m - (ns + m - 1) # Degrees of freedom
+  if (is.null(add.es)) {
+    if (choose.model) {
+      if (!is.na(rep)) {
+        model.no.int <- lme(y~met, data = data.long, random = ~1|id, method = "REML")
+        model.int <- lme(y~met, data = data.long,
+                         random = list(id = pdBlocked(list(~1, pdIdent(form = ~-1+met)))),
+                         method = "REML")
+        if (AIC(model.no.int) <= AIC(model.int)) {
+          model <- model.no.int
+          md <- abs(summary(model)$coefficients$fixed[2])
+          sd <- sqrt(2)*model$sigma
+          df <- 2*ns*m - (ns + m - 1)
+        } else{
+          model <- model.int
+          md <- abs(summary(model)$coefficients$fixed[2])
+          var.int <- getVarCov(model)[2,2]
+          sd <- sqrt(2*(var.int + model$sigma^2))
+          df <- 2*ns*(m - 1)
+        }
       } else{
-        model <- model.int
+        model <- lme(y~met, data = data.long, random = ~1|id, method = "REML")
         md <- abs(summary(model)$coefficients$fixed[2])
-        var.int <- getVarCov(model)[2,2]
-        sd <- sqrt(2*(var.int + model$sigma^2)) # Standard deviation of the difference
-        df <- 2*ns*(m - 1) # Degrees of freedom
+        sd <- sqrt(2)*model$sigma
+        df <- 2*ns*m - (ns + m - 1)
       }
-    } else{
+    } else if (int == F) {
       model <- lme(y~met, data = data.long, random = ~1|id, method = "REML")
       md <- abs(summary(model)$coefficients$fixed[2])
-      sd <- sqrt(2)*model$sigma # Standard deviation of the difference
-      df <- 2*ns*m - (ns + m - 1) # Degrees of freedom
+      sd <- sqrt(2)*model$sigma
+      df <- 2*ns*m - (ns + m - 1)
+    } else{
+      model <- lme(y~met, data = data.long, random = list(id = pdBlocked(list(~1,pdIdent(form = ~-1+met)))), method = "REML")
+      md <- abs(summary(model)$coefficients$fixed[2])
+      var.int <- getVarCov(model)[2,2]
+      sd <- sqrt(2*(var.int + model$sigma^2))
+      df <- 2*ns*(m - 1)
     }
-  } else if(int == F){
-    model <- lme(y~met, data = data.long, random = ~1|id, method = "REML")
-    md <- abs(summary(model)$coefficients$fixed[2])
-    sd <- sqrt(2)*model$sigma # Standard deviation of the difference
-    df <- 2*ns*m - (ns + m - 1) # Degrees of freedom
   } else{
-    model <- lme(y~met, data = data.long, random = list(id = pdBlocked(list(~1,pdIdent(form = ~-1+met)))), method = "REML")
-    md <- abs(summary(model)$coefficients$fixed[2])
-    var.int <- getVarCov(model)[2,2]
-    sd <- sqrt(2*(var.int + model$sigma^2)) # Standard deviation of the difference
-    df <- 2*ns*(m - 1) # Degrees of freedom
+    formula_str <- paste0("y ~ met + ", paste(add.es, collapse = " + "))
+    formula_obj <- as.formula(formula_str)
+    if (choose.model) {
+      if (!is.na(rep)) {
+        model.no.int <- lme(formula_obj, data = data.long, random = ~1|id, method = "REML")
+        model.int <- lme(formula_obj, data = data.long,
+                         random = list(id = pdBlocked(list(~1, pdIdent(form = ~-1+met)))),
+                         method = "REML")
+        if (AIC(model.no.int) <= AIC(model.int)) {
+          model <- model.no.int
+          md <- abs(summary(model)$coefficients$fixed[2])
+          sd <- sqrt(2)*model$sigma
+          df <- 2*ns*m - (ns + m - 1)
+        } else{
+          model <- model.int
+          md <- abs(summary(model)$coefficients$fixed[2])
+          var.int <- getVarCov(model)[2,2]
+          sd <- sqrt(2*(var.int + model$sigma^2))
+          df <- 2*ns*(m - 1)
+        }
+      } else{
+        model <- lme(formula_obj, data = data.long, random = ~1|id, method = "REML")
+        md <- abs(summary(model)$coefficients$fixed[2])
+        sd <- sqrt(2)*model$sigma
+        df <- 2*ns*m - (ns + m - 1)
+      }
+    } else if (int == F) {
+      model <- lme(formula_obj, data = data.long, random = ~1|id, method = "REML")
+      md <- abs(summary(model)$coefficients$fixed[2])
+      sd <- sqrt(2)*model$sigma
+      df <- 2*ns*m - (ns + m - 1)
+    } else{
+      model <- lme(formula_obj, data = data.long, random = list(id = pdBlocked(list(~1, pdIdent(form = ~-1+met)))), method = "REML")
+      md <- abs(summary(model)$coefficients$fixed[2])
+      var.int <- getVarCov(model)[2,2]
+      sd <- sqrt(2*(var.int + model$sigma^2))
+      df <- 2*ns*(m - 1)
+    }
   }
 
   # tdi
   nc <- md/sd
-  tdi <- sd*sqrt(qchisq(p,1,ncp = (nc^2)))
+  tdi.hat <- sd*sqrt(qchisq(p, 1, ncp = (nc^2)))
 
-  # Computing p1 using binary search algorithm
-  low <- p
-  high <- 1
-  while (low <= high){
-    mid <- (high + low)/2
-    p.est <- pnorm(qnorm(mid)) - pnorm(-qnorm(mid)-2*md/sd) - p
-    if ((p.est <= tol)&(p.est >= -tol)){
-      break
+  ub.hat <- NA
+  if(ub){
+    # computing p1 using binary search algorithm
+    low <- p
+    high <- 1
+    while (low <= high){
+      mid <- (high + low)/2
+      p.est <- pnorm(qnorm(mid)) - pnorm(-qnorm(mid)-2*md/sd) - p
+      if ((p.est <= tol) & (p.est >= -tol)){
+        break
+      }
+      if (p.est > tol){
+        high <- mid - tol
+      }
+      if (p.est < (-tol)){
+        low <- mid + tol
+      }
     }
-    if (p.est > tol){
-      high <- mid - tol
-    }
-    if (p.est < (-tol)){
-      low <- mid + tol
-    }
+
+    # ub
+    n <- 2*m*ns
+    k <- suppressWarnings(1/sqrt(n)*qt(1-alpha, df, ncp = (qnorm(mid)*sqrt(n))))
+    ub.hat <- md + k*sd
   }
-
-  # ub
-  n <- 2*m*ns
-  k <- suppressWarnings(1/sqrt(n)*qt(1-alpha, df, ncp = (qnorm(mid)*sqrt(n))))
-  ub <- md + k*sd
-  result <- c(tdi, ub)
+  result <- c(tdi.hat, ub.hat)
   out <- list(result = result, fitted.model = model)
   return(out)
 }
--- R/print.tdi.R
+++ R/print.tdi.R
@@ -5,26 +5,48 @@
 #' @param x input object of class \code{tdi} resulting from a call of the function \code{\link[TDIagree]{TDI}}.
 #' @param ... currently not in use
 #' @returns A nice \strong{gt} table containing the values computed with the function \code{\link[TDIagree]{TDI}}.
-#'          The number of decimals for the estimates and the probabilities correspond to the arguments \code{dec.est} and \code{dec.p} of the function \code{\link[TDIagree]{TDI}}, respectively.
+#'          The number of decimals for the estimates and the proportions correspond to the arguments \code{dec.est} and \code{dec.p} of the function \code{\link[TDIagree]{TDI}}, respectively.
 #'
 #' @examples
+#' # normal data
+#'
 #' set.seed(2025)
 #'
 #' n <- 100
-#' y_A1 <- rnorm(n, 50, 10) # rater A, replicate 1
-#' y_A2 <- rnorm(n, 50, 10) # rater A, replicate 2
-#' y_B1 <- rnorm(n, 30, 15) # rater B, replicate 1
-#' y_B2 <- rnorm(n, 30, 15) # rater B, replicate 2
 #'
-#' ex_data <- data.frame(y = c(y_A1, y_A2, y_B1, y_B2), rater = factor(rep(c("A", "B"), each = 2*n)),
-#'                       replicate = factor(rep(rep(1:2, each = n), 2)), subj = factor(rep(1:n, 4)))
+#' mu.ind <- rnorm(n, 0, 7)
+#'
+#' epsA1 <- rnorm(n, 0, 3)
+#' epsA2 <- rnorm(n, 0, 3)
+#' epsB1 <- rnorm(n, 0, 3)
+#' epsB2 <- rnorm(n, 0, 3)
+#'
+#' y_A1 <- 50 + mu.ind + epsA1 # rater A, replicate 1
+#' y_A2 <- 50 + mu.ind + epsA2 # rater A, replicate 2
+#' y_B1 <- 40 + mu.ind + epsB1 # rater B, replicate 1
+#' y_B2 <- 40 + mu.ind + epsB2 # rater B, replicate 2
+#'
+#' ex_data <- data.frame(y = c(y_A1, y_A2, y_B1, y_B2),
+#'                       rater = factor(rep(c("A", "B"), each = 2*n)),
+#'                       replicate = factor(rep(rep(1:2, each = n), 2)),
+#'                       subj = factor(rep(1:n, 4)))
 #'
 #' tdi <- TDI(ex_data, y, subj, rater, replicate, p = c(0.8, 0.9),
-#'            method = c("Choudhary P", "Escaramis", "Choudhary NP", "Perez-Jaume and Carrasco"))
+#'            method = c("Choudhary P", "Escaramis et al.",
+#'                       "Choudhary NP", "Perez-Jaume and Carrasco"),
+#'            boot.type = "cluster", R = 1000)
 #' tdi
 #'
 #'
-#' @importFrom gt gt fmt_number tab_spanner md ends_with tab_footnote cells_column_labels starts_with cols_label cols_align cols_width px tab_style cell_borders cells_body
+#' # non-normal data
+#'
+#' tdi.aml <- TDI(AMLad, mrd, id, met, rep, p = c(0.85, 0.95), boot.type = "cluster",
+#'                dec.est = 4, R = 1000)
+#' tdi.aml
+#'
+#'
+#' @importFrom gt gt fmt_number tab_spanner md contains ends_with tab_footnote cells_column_labels starts_with cols_label cols_align cols_width px tab_style cell_borders cells_body
+#' @importFrom katex katex_html
 #'
 #' @exportS3Method TDIagree::print
 
@@ -36,6 +58,10 @@ print.tdi <- function(x, ...) {
 
   dec.est <- x$params$dec.est
   dec.p <- x$params$dec.p
+  # alpha <- x$params$alpha
+  # perc <- round((1-alpha)*100, 0)
+  # ub.text <- paste0("$\\text{UB}_{", perc, "\\%}(\\hat{\\kappa}_p)$")
+
 
   n <- nrow(x$result)
   if(n > 1){
@@ -44,45 +70,62 @@ print.tdi <- function(x, ...) {
     white_rows <- F
   }
 
+  footnote <- "$p$, proportion; $\\hat{\\kappa}_p$, TDI estimate"
+
+  if (x$params$ub) {
+    footnote <- paste0(footnote, "; $\\text{UB}$, upper bound")
+    if ("perez-jaume and carrasco" %in% x$params$method) {
+      footnote <- paste0(footnote, "; $\\text{p}$, strategy based on percentiles; $\\text{n}$, strategy based on the normal distribution; $\\text{e}$, strategy based on the empirical quantiles; $\\text{BC}_a$, strategy based on the bias-corrected and accelerated method")
+    }
+  }
+
   result <- x$result |>
     gt() |>
     fmt_number(columns = -"p", decimals = dec.est) |>
     fmt_number(columns = "p", decimals = dec.p) |>
-    tab_spanner(label = md("**Lin**"),
-                columns = ends_with("lin.p")) |>
+    tab_spanner(label = md("**Differences bootstrap**"),
+                columns = contains("_db")) |>
+    tab_spanner(label = md("**Cluster bootstrap**"),
+                columns = contains("_cb")) |>
     tab_spanner(label = md("**Choudhary P**"),
-                columns = ends_with("ch.p")) |>
-    tab_spanner(label = md("**Escaramis**"),
-                columns = ends_with("es.p")) |>
+                columns = ends_with("ch.p"),
+                level = 2) |>
+    tab_spanner(label = md("**Escaramis *et al.***"),
+                columns = ends_with("es.p"),
+                level = 2) |>
     tab_spanner(label = md("**Choudhary NP**"),
-                columns = ends_with("ch.np")) |>
+                columns = ends_with("ch.np"),
+                level = 2) |>
     tab_spanner(label = md("**Perez-Jaume and Carrasco**"),
-                columns = ends_with("pc.np")) |>
+                columns = ends_with("pc.np"),
+                level = 2) |>
     tab_spanner(label = md("**Parametric methods**"),
                 columns = ends_with(".p")) |>
     tab_spanner(label = md("**Non-parametric methods**"),
                 columns = ends_with(".np")) |>
-    tab_footnote(footnote = "Total deviation index estimate",
-                 locations = cells_column_labels(columns = starts_with("tdi"))) |>
-    tab_footnote(footnote = "95% upper bound",
-                 locations = cells_column_labels(columns = starts_with("ub."))) |>
-    tab_footnote(footnote = "95% upper bound for the bootstrap non-parametric technique based on percentiles",
-                 locations = cells_column_labels(columns = starts_with("ub_p"))) |>
-    tab_footnote(footnote = "95% upper bound for the bootstrap non-parametric technique based on the normal distribution",
-                 locations = cells_column_labels(columns = starts_with("ub_nlog"))) |>
-    cols_label("p" ~ md("*p*"),
-               starts_with("tdi") ~ md("*{{:kappa:_p}}*"),
-               starts_with("ub") ~ "{{UB_95%}}",
-               starts_with("ub_p.pc.np") ~ "{{UB[_95%^P]}}",
-               starts_with("ub_nlog.pc.np") ~ "{{UB[_95%^N]}}") |>
+    # cols_label(p ~ md("$p$"),
+    #            starts_with("tdi") ~ md("$\\hat{\\kappa}_p$"),
+    #            starts_with("ub") ~ md("$\\text{UB}_{95\\%}(\\kappa}_p)$"),
+    #            contains("_p") ~ md("$\\text{UB}_{95\\%}^{\\text{p}}(\\kappa_p)$"),
+    #            contains("_n") ~ md("$\\text{UB}_{95\\%}^{\\text{n}}(\\kappa_p)$"),
+    #            contains("_e") ~ md("$\\text{UB}_{95\\%}^{\\text{e}}(\\kappa_p)$"),
+    #            contains("_b") ~ md("$\\text{UB}_{95\\%}^{\\text{BC}_a}(\\kappa_p)$")) |>
+    cols_label(p ~ md("$p$"),
+               starts_with("tdi") ~ md("$\\hat{\\kappa}_p$"),
+               starts_with("ub") ~ md("$\\text{UB}(\\kappa_p)$"),
+               contains("_p") ~ md("$\\text{UB}^{\\text{p}}(\\kappa_p)$"),
+               contains("_n") ~ md("$\\text{UB}^{\\text{n}}(\\kappa_p)$"),
+               contains("_e") ~ md("$\\text{UB}^{\\text{e}}(\\kappa_p)$"),
+               contains("_b") ~ md("$\\text{UB}^{\\text{BC}_a}(\\kappa_p)$")) |>
     cols_align("center") |>
-    cols_width(everything() ~ px(80)) |>
+    cols_width(everything() ~ px(105)) |>
     tab_style(style = cell_borders(sides = c("right"),
                                    weight = px(2), color = "lightgrey"),
                locations = cells_body(columns = "p")) |>
     tab_style(style = cell_borders(sides = c("top"),
                                    weight = px(2), color = "white"),
-              locations = cells_body(rows = white_rows))
+              locations = cells_body(rows = white_rows)) |>
+    tab_footnote(md(footnote))
 
   print(result)
 }
